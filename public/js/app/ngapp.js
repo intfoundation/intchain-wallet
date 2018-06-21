3ea4227ff01f063b6d84de4723885a4df5cd64a9
e <1403049908@qq.com>
2018-06-21 16:13:26 +0800

    check in

--- public/js/app/ngapp.js
+++ public/js/app/ngapp.js
@@ -1,4 +1,1 @@
 ﻿var app = angular.module('myApp', ['ngRoute', 'ngSanitize', 'ngAnimate', 'angularFileUpload']);
-// app.config(function($httpProvider) {
-//     $httpProvider.defaults.withCredentials = true;
-// });
